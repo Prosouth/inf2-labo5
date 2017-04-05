7df1f0f179d1a48ec54413dd75ad500502fc0481
Walid Koubaa <walidkoubaa@mbp-de-walid.einet.ad.eivd.ch>
2017-04-05 16:41:37 +0200

    Initial Commit

---
+++ inf2-labo5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inf2-labo5
+//
+//  Created by Walid Koubaa on 05.04.17.
+//  Copyright © 2017 Walid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
